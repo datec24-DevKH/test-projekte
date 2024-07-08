33d1ad075b932df3f661ad29336dae20db637fd8
datec24-DevKH <196azura@gmail.com>
2024-07-08 10:45:36 +0200

    geht

--- bewerbungsformular_v2/src/app/app.module.ts
+++ bewerbungsformular_v2/src/app/app.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFireModule } from '@angular/fire/compat';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { environment } from '../environments/environment.development';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { BodyComponent } from './body/body.component';
 import { TestComponent } from './test/test.component';
 
 
@@ -23,7 +16,6 @@ import { TestComponent } from './test/test.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent,
     TestComponent,
 
   ],
@@ -32,16 +24,13 @@ import { TestComponent } from './test/test.component';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireStorageModule,
+
     
   ],
   providers: [
     provideHttpClient(withFetch()),
     provideClientHydration(),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore()),
-    provideAnimationsAsync(),
+
   ],
   bootstrap: [AppComponent]
 })
--- bewerbungsformular_v2/src/environments/environment.development.ts
+++ bewerbungsformular_v2/src/environments/environment.development.ts
@@ -1,16 +1,5 @@
 export const environment = {
-        // Your web app's Firebase configuration
 
-        firebase: {
-
-          apiKey: "AIzaSyAnaQ-igqZf6wgSjUD47SpmEpk8GopdjsY",
-          authDomain: "bewerbungsformular-54021.firebaseapp.com",        
-          projectId: "bewerbungsformular-54021",        
-          storageBucket: "bewerbungsformular-54021.appspot.com",       
-          messagingSenderId: "831004135307",        
-          appId: "1:831004135307:web:a251fa0469d0bddf8ce98e"
-        
-        },
         personio: {
 
           apiUrl: 'https://api.personio.de/v1/auth',
